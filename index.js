c7b59dbe80646c11153c52202479b0a66c030a89
eedamame <eedamame@gmail.com>
2017-02-28 12:52:02 +0900

    get javascript object with necessary information

--- index.js
+++ index.js
@@ -1,76 +1,70 @@
+// tools
 const postcss = require('postcss');
 const util = require('util');
 const fs = require('fs');
-const Comb = require('csscomb');
+const glob = require('glob');
 
+// セレクタ情報突っ込む配列
+var check = [];
 
+const targetFiles = './css/*.css';
 
+glob( targetFiles, function(err, files) {
+  files.forEach(function(file) {
+    const css = fs.readFileSync(file);
+    const root = postcss.parse(css);
 
+    root.walk(function (rule) {
 
-//const css = fs.readFileSync('./css/matching.css');
-var css = fs.readFileSync('./css/bootstrap.min.css');
-
-var comb = new Comb('zen');
-comb.processPath('./css');
-
-const root = postcss.parse(css);
-
-//console.log(util.inspect(root, false, null))
-
-//console.log(root);
-
-const check = [];
-
-root.walk(function (rule) {
-  //console.log('rule: ' + rule + '\n');
-  //if(rule.selector) {
-  //  console.log('selector: ' + rule.selector + ' ');
-  //  console.log('line: ' + rule.source.start.line + '\n');
-  //}
-
-  const slct = rule.selector;
-
-  if(slct) {
-    var ruleItem;
-
-    // 複数セレクタの場合は、分割する
-    if(slct.match(/,/)) {
-      const splitSelectors = slct.split(',');
-      console.log('splitSelectors', splitSelectors);
-      for(var i = 0; i<splitSelectors.length; i++) {
-        console.log(splitSelectors[i])
-        ruleItem = {
-          'selector': splitSelectors[i],
-          'line': rule.source.start.line
+      const slct = rule.selector;
+      if(slct) {
+        var ruleItem;
+        var thisAtRule = '';
+        if(rule.parent.type === 'atrule') {
+          thisAtRule = rule.parent.name + ' ' + rule.parent.params;// nameで、'media' とかが入る。params で、指定の内容が入る 'only screen and (min-width:768px)' とか
         }
-        check.push(ruleItem);
+
+        // 複数セレクタの場合は、分割する
+        if(slct.match(/,/)) {
+          const splitSelectors = slct.split(',');
+
+          //console.log('splitSelectors', splitSelectors);
+          for(var i = 0; i<splitSelectors.length; i++) {
+            //console.log(splitSelectors[i])
+            ruleItem = {
+              'selector': splitSelectors[i],
+              'line': rule.source.start.line,
+              'file': file,
+              'atrule': thisAtRule
+            }
+            //console.log('L35 ruleItem: ', ruleItem);
+            check.push(ruleItem);
+          }
+        } else {
+          ruleItem = {
+            'selector': rule.selector,
+            'line': rule.source.start.line,
+            'file': file,
+            'atrule': thisAtRule
+          }
+          //console.log('L43 ruleItem: ', ruleItem);
+          check.push(ruleItem);
+        }
       }
-    } else {
-      ruleItem = {
-        'selector': rule.selector,
-        'line': rule.source.start.line
-      }
-      check.push(ruleItem);
-    }
+    });
 
+  })
 
+  // selectorのアルファベット順にソート
+  check.sort(function(a,b) {
+    if(a.selector < b.selector) return -1;
+    if(a.selector > b.selector) return 1;
+    return 0;
+  });
 
-    //check[rule.selector] = {};
-    //check[rule.selector].selector = rule.selector;
-    //check[rule.selector].line = rule.source.start.line;
-  }
+  // ファイルに書き込み
+  fs.writeFile('writetest.txt', util.inspect(check) , function (err) {
+    console.log(err);
+  });
+
 });
-
-// selectorのアルファベット順にソート
-check.sort(function(a,b) {
-  if(a.selector < b.selector) return -1;
-  if(a.selector > b.selector) return 1;
-  return 0;
-});
-//console.log(check)
-
-
-fs.writeFile('writetest.txt', util.inspect(check) , function (err) {
-  console.log(err);
-});
-
